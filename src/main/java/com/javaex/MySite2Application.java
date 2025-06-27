ae41265c09995d8757856b2e712e8d2a972b9c3f
ahah2025 <adghty61@naver.com>
2025-06-27 14:25:24 +0900

    2025-06-27

---
+++ src/main/java/com/javaex/MySite2Application.java
@@ -0,0 +1,13 @@
+package com.javaex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySite2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySite2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaex/MySite2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySite2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
