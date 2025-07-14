3cb7b99c7e7fb4ab746a95b05f94f45238b1964d
ahah2025 <adghty61@naver.com>
2025-07-14 14:09:19 +0900

    2025-07-14

---
+++ src/test/java/com/javaex/repository/GuestbookRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.javaex.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.javaex.vo.GuestbookVO;
+
+@SpringBootTest
+public class GuestbookRepositoryTest {
+
+	// 필드
+	@Autowired
+	private GuestbookRepository guestbookRepository;
+
+	// 전체데이터 가져오기
+	@Test
+	public void selectAll() {
+		List<GuestbookVO> guestbookList = guestbookRepository.guestbookSelect();
+		System.out.println("---------------------------------------------------");
+		System.out.println(guestbookList);
+		System.out.println("---------------------------------------------------");
+		
+		assertThat(guestbookList).isNotNull();
+	}
+}
