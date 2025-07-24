d784f2d768b98f052d371173945da13f55ed6938
ahah2025 <adghty61@naver.com>
2025-07-24 21:29:44 +0900

    0930

--- src/main/java/com/javaex/repository/FileRepository.java
+++ src/main/java/com/javaex/repository/FileRepository.java
@@ -2,9 +2,11 @@ package com.javaex.repository;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.javaex.vo.FileVO;
 
+@Repository
 public class FileRepository {
 
 	//필드
